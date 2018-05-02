492865f21cd07ad1b8b6e0f86a7701e5e4708801
TruongTX <me@truongtx.me>
2018-05-02 20:08:35 +0700

    Implement union function

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,16 +1,34 @@
-public class Percolation {
-    private int[] id;
+import java.util.Arrays;
 
-    private int n;
+public class Percolation {
+    // store the site data
+    public int[] sites;
+
+    // store the data (root)
+    public int[] id;
+
+    // store the tree weight
+    public int[] weight;
+
+    public int n;
 
     public Percolation(int n) {
         this.n = n;
         int total = n * n;
+
+        sites = new int[total];
         id = new int[total];
+        weight = new int[total];
 
         for (int i = 0; i < total; i++) {
             // init all sites to be blocked
-            id[i] = 0;
+            sites[i] = 0;
+
+            // init all sites to point to itself
+            id[i] = i;
+
+            // init all tree to have weight 1
+            weight[i] = 1;
         }
     }
 
@@ -19,19 +37,48 @@ public class Percolation {
     }
 
     public boolean isOpen(int row, int col) {
-
+        return true;
     }
 
     public boolean isFull(int row, int col) {
-
+        return true;
     }
 
     public int numberOfOpenSites() {
-
+        return 1;
     }
 
     public boolean percolates() {
-        
+        return true;
+    }
+
+    public void union(int p, int q) {
+        int rootP = this.root(p);
+        int rootQ = this.root(q);
+        int weightP = weight[rootP];
+        int weightQ = weight[rootQ];
+
+        if (weightP >= weightQ) {
+            id[rootQ] = rootP;
+            weight[rootP] += weight[rootQ];
+        } else {
+            id[rootP] = rootQ;
+            weight[rootQ] += weight[rootP];
+        }
+    }
+
+    /**
+     * Find root of one item
+     *
+     * @param i
+     * @return
+     */
+    private int root(int i) {
+        while (i != id[i]) {
+            i = id[i];
+        }
+
+        return i;
     }
 
     private int convertToIndex(int row, int col) {
@@ -39,6 +86,14 @@ public class Percolation {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Percolation per = new Percolation(5);
+
+        per.union(10, 15);
+        per.union(12, 21);
+        per.union(10, 20);
+        per.union(10, 24);
+        per.union(24, 2);
+
+        System.out.println(Arrays.toString(per.id));
     }
 }
