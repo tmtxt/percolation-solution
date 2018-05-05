092779dfa3799ee46abb86cb83ed154dac214925
TruongTX <me@truongtx.me>
2018-05-05 10:21:06 +0700

    Add basic implementation for running percolation

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,4 +1,15 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+import java.util.Arrays;
+
 public class PercolationStats {
+
+    private int n;
+    private int total;
+    private int trials;
+    private double[] res;
+
     public PercolationStats(int n, int trials) {
         if (n <= 0) {
             throw new IllegalArgumentException("n must be greater than 0");
@@ -7,6 +18,39 @@ public class PercolationStats {
         if (trials <= 0) {
             throw new IllegalArgumentException("trials must be greater than 0");
         }
+
+        this.n = n;
+        this.total = n * n;
+        this.trials = trials;
+        this.res = new double[trials];
+
+        this.runAll();
+    }
+
+    // run all trials times
+    private void runAll() {
+        for (int i = 0; i < trials; i++) {
+            this.runOne(i);
+        }
+    }
+
+    private void runOne(int order) {
+        Percolation percolation = new Percolation(n);
+        int count = 0;
+
+        do {
+            int row = StdRandom.uniform(1, this.n + 1);
+            int col = StdRandom.uniform(1, this.n + 1);
+
+            if (percolation.isOpen(row, col)) {
+                continue;
+            }
+
+            percolation.open(row, col);
+            count++;
+        } while (!percolation.percolates());
+
+        this.res[order] = (double) count / this.total;
     }
 
     public double mean() {
@@ -26,6 +70,7 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
-
+        PercolationStats stat = new PercolationStats(5, 2);
+        System.out.println(Arrays.toString(stat.res));
     }
 }
